61023dc628ffbe6f43562de60b3fa8cd34822c8a
jerry015 <42257913+jerry015@users.noreply.github.com>
2018-12-25 14:56:24 +0800

    Update AutoZIP.py

--- AutoZIP.py
+++ AutoZIP.py
@@ -3,34 +3,84 @@
 import os
 import subprocess
 import time
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import PySimpleGUI as sg
 
-fname = 'Jingui'
-lname = 'Zhang'
-files_path = 'D:\\bb'
-rarname = r"D:\test"
+source_file = sg.PopupGetFile("请选择数据源文件：")
+wb = load_workbook(filename=source_file)
+# grab the active worksheet
+ws = wb.active
+fnamelist = []
+lnamelist = []
+files_pathlist = []
+# rarfilepath = r'C:\Users\vicky.hongmei.lu\Desktop\'
+rarfilepath = sg.PopupGetFolder('请选择存放压缩包的文件夹：')
+# print(rarfilepath)
+error_name = []
+
+for i in range(2,100):
+	if ws['G'+str(i)].value:
+		fnamelist.append(ws['G'+str(i)].value.split(' ')[1])
+		lnamelist.append(ws['G'+str(i)].value.split(' ')[0])
+		files_pathlist.append(ws['Q'+str(i)].value)
+	else:
+		break
 
 
-# # 新建压缩包，放文件进去,若压缩包已经存在，将覆盖。可选择用a模式，追加
-# azip = zipfile.ZipFile(r'D:\bb\bb.zip', 'w')
+        # print(ws['G'+str(i)].value)
+for i in range(len(fnamelist)):
+	# print(fnamelist[i], lnamelist[i], 'T:'+files_pathlist[i][33:]+ '\\')
+	fname = fnamelist[i]
+	lname = lnamelist[i]
+	files_path = 'T:'+files_pathlist[i][33:]+ '\\'
+	# files_path = r"C:\bb\cc"
+
+	rarfullname = rarfilepath +'\\'+ lname + ' ' +fname + '.rar'
+	rarcmd = r'C:\Program Files\WinRAR\WinRAR.exe'
+	rarpasswd = lname+'!'+fname
+
+	filecount = 0
+	for current_path, subfolders, filesname in os.walk(files_path):
+		for file in filesname:
+			# print(os.path.join(current_path, file))
+			if ("Offer Letter_" in file) and (".pdf" in file):
+				filecount = filecount + 1
+				cmd = [rarcmd, 'a', '-ep', '-hp'+rarpasswd, rarfullname, os.path.join(current_path, file)]
+				sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+				time.sleep(1)
+			if ("Employment Contract" in file) and ('.pdf' in file):
+				filecount = filecount + 1
+				cmd = [rarcmd, 'a', '-ep', '-hp'+rarpasswd, rarfullname, os.path.join(current_path, file)]
+				sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+				time.sleep(1)
+			if ('Cover' in file) and ('.pdf' in file):
+				filecount = filecount + 1
+				cmd = [rarcmd, 'a', '-ep', '-hp'+rarpasswd, rarfullname, os.path.join(current_path, file)]
+				sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+				time.sleep(1)
+			if ('Checklist' in file) and ('.pdf' in file):
+				filecount = filecount + 1
+				cmd = [rarcmd, 'a', '-ep', '-hp'+rarpasswd, rarfullname, os.path.join(current_path, file)]
+				sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+				time.sleep(1)
+			if ('form' in file) and ('.xlsx' in file):
+				filecount = filecount + 1
+				cmd = [rarcmd, 'a', '-ep', '-hp'+rarpasswd, rarfullname, os.path.join(current_path, file)]
+				sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+				time.sleep(1)
+	if filecount == 5:
+		filecount = 0
+	else:
+		error_name.append(lname + fname)
 
 
-# for current_path, subfolders, filesname in os.walk(r'D:\bb'):
-#     print(current_path, subfolders, filesname)
-#     #  filesname是一个列表，我们需要里面的每个文件名和当前路径组合
-#     for file in filesname:
-#         # 将当前路径与当前路径下的文件名组合，就是当前文件的绝对路径
-#         azip.write(os.path.join(current_path, file))
 
-# # 关闭资源
-# azip.close()
-
-# azip.setpassword(lname+'!'+fname)
-
-
-
-for current_path, subfolders, filesname in os.walk(files_path):
-    for file in filesname:
-        print(os.path.join(current_path, file))
-        cmd = [r'C:\Program Files\WinRAR\WinRAR.exe', 'a', '-hp'+lname+'!'+fname, rarname, os.path.join(current_path, file)]
-        sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-        time.sleep(5) 
+if len(error_name) != 0:
+	error_name_list = ''
+	for e in error_name:
+		error_name_list = error_name_list + e + '\n'
+	sg.PopupError('以下候选人文件确实，请手工处理：\n' + error_name_list)
+else:
+	sg.PopupOK("压缩包全部生成成功，请检查！")
+			
